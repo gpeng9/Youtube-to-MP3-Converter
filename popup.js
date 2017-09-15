468a289436cfe04f443052ce250e659d69785b82
gpeng9 <gracepeng2007@yahoo.com>
2017-09-15 11:45:30 -0700

    First commit

--- popup.js
+++ popup.js
@@ -1,34 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-	var checkPageButton = document.getElementById('copyLink');
-	checkPageButton.addEventListener('click', function() {
-		chrome.tabs.getSelected(null, function(tab) {
-			d = document;
-			var f = d.createElement('form');
-			f.action = 'http://www.flvto.biz/';
-			f.method = 'post';
-			var i = d.createElement('input');
-			i.type = 'hidden';
-			i.name = 'url';
-			i.value = tab.url;
-			f.appendChild(i);
-			d.body.appendChild(f);
-			f.submit();
-		});
-	}, false);
-}, false);*/
-
-/*
-function update() {
-	var video_id = window.location.search.split('v=')[1];
-	var ampersandPosition = video_id.indexOf('&');
-	if (ampersandPosition != -1)
-	{
-		video_id = video_id.substring(0, ampersandPosition);
-	}
-	alert(video_id);
-	//document.getElementById('myIframe').setAttribute('src', "https://www.yt-download.org/api-console/audio/" + video_id);
-	document.getElementById('myIframe').src = "https://www.yt-download.org/api-console/audio/" + video_id;
-}*/
 function update()
 {
 	chrome.tabs.getSelected(null, function(tab) {
